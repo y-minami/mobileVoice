8937a38b4316e4557b9bed400824bb4adba4d67a
Yohei Minami <mi73@me.com>
2017-10-14 20:14:14 +0900

    commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+const STATIC_ROOT = './public';
+
+// DEV SERVER
+gulp.task('server', function () {
+  browserSync.init({
+    port: 3010,
+    server: {
+      baseDir: STATIC_ROOT
+    }
+  });
+
+  gulp.watch(STATIC_ROOT + '/**/*', function() {
+
+    browserSync.reload();
+  });
+});
